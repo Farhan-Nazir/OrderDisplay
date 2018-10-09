05a03a59ee5a77ae7cacf6794f099130dabeac62
Farhan Nazir <nazirf3440@gmail.com>
2018-10-09 10:53:17 +0200

    OrderApp Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Orders from "./components/orders";
 
 class App extends Component {
+  callItem() {
+    this.refs.orders.btnAdd();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button type="submit" onClick={this.callItem.bind(this)}>
+          Add
+        </button>
+        <Orders ref="orders" />
       </div>
     );
   }
---
+++ src/components/orders.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./orders.css";
+class Orders extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: 1
+    };
+  }
+  btnAdd() {
+    let inprogressItems = document.getElementById("inprogress-items");
+    let newDiv = document.createElement("button");
+    this.setState(prevState => {
+      return { orders: prevState.orders + 1 };
+    });
+    newDiv.id = this.state.orders;
+
+    newDiv.appendChild(
+      document.createTextNode("Order #: " + this.state.orders)
+    );
+    newDiv.onclick = () => {
+      let readyItems = document.getElementById("readyItems");
+      let inprogressItem = document.getElementById(newDiv.id);
+      inprogressItem.parentNode.removeChild(inprogressItem);
+      let readyItemDiv = document.createElement("button");
+      readyItemDiv.id = inprogressItem.id;
+      readyItemDiv.appendChild(
+        document.createTextNode(inprogressItem.textContent)
+      );
+      readyItemDiv.onclick = () => {
+        let deletDiv = document.getElementById(readyItemDiv.id);
+        readyItems.removeChild(deletDiv);
+      };
+      readyItems.appendChild(readyItemDiv);
+      console.log(inprogressItem);
+    };
+    inprogressItems.appendChild(newDiv);
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="orderProgress">
+          <h1>Order Started </h1>
+          <div className="inprogress-items" id="inprogress-items" />
+        </div>
+        <div className="pageDivider" />
+        <div className="orderCompleted">
+          <h1>Order is Ready</h1>
+          <div className="ready-items" id="readyItems" />
+        </div>
+      </div>
+    );
+  }
+}
+export default Orders;
